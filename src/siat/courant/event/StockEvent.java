fa1dd80364b965ec005a70b5cecd8d799a6a78a0
PeterXUYAOHAI <peterxuyaohai@gmail.com>
2015-06-03 22:37:20 +0800

    Add StockEvent Add StockEventFactory

---
+++ src/siat/courant/event/StockEvent.java
@@ -0,0 +1,76 @@
+
+public class StockEvent implements Event{
+	
+	long timestamp;
+	long id;
+	String EventType="stock";
+	long volume;
+	long price;
+	long symbol;
+	String stockMarket;
+	
+	public StockEvent(long timestamp, long id, long volume, long price, long symbol, String stockMarket){
+		this.timstamp=timestamp;
+		this.id=id;
+		this.volume=volume;
+		this.price=price;
+		this.symbol=symbol;
+		this.stockMarkeet=stockMarket;
+	}
+	
+	public long getTimestamp(){
+		return timestamp;
+	}
+	
+	public long getId(){
+		return id;
+	}
+	
+	public String getEventType(){
+		return eventType;
+	}
+	
+	public void setId(long id){
+		this.id=id;
+	}
+	
+	public void setTimestamp(long timestamp){
+		this.timestamp=timestamp;
+	}
+	
+	public void setVolume(long volume){
+		this.volume=volume;
+	}
+	
+	public void setPrice(long price){
+		this.price=price;
+	}
+	
+	public void setSymbol(long symbol){
+		this.symbol=symbol;
+	}
+	
+	public void setStockMarket(String stockMarket){
+		this.stockMarket=stockMarket;
+	}
+	
+	public long getAttributeByName(String attributeName){
+		if(attributeName.equals('volume'))
+			return volume;
+		else if(attributeName.equals('price'))
+			return price;
+		else if(attributeName.equals('symbol'))
+			return symbol;
+		else if(attributeName.equals('Timestamp'))
+			return timestamp;
+		else if(attributeName.equals('id'))
+			return id;
+		return null;
+	}
+	
+	public String toString(){
+		return "ID = "+ id + "\tTimestamp = " + timestamp
+				+ "\tSymbol = " + this.symbol + "\tStockMarket" + stockMarket + "\tPrice = " + price + "\tVolume = " + volume;
+		
+	}
+}
---
+++ src/siat/courant/event/StockEventFactory.java
@@ -0,0 +1,15 @@
+
+public class stockEventFactory extends EventFactory{
+	public stockEvent createEvent(String type)
+	{
+			return new stockEvent(10800, 0, 10, 10, 5, SH);
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
