3f128f77716d5aa84c73288ef2f69d9127857027
PeterXUYAOHAI <peterxuyaohai@gmail.com>
2015-06-16 13:14:23 +0800

    some modify

---
+++ tests/siat/courant/stream/BlockingQueueStreamTest.java
@@ -0,0 +1,27 @@
+package siat.courant.stream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by asus on 2015/6/15.
+ */
+public class BlockingQueueStreamTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testPopEvent() throws Exception {
+
+    }
+
+    @Test
+    public void testAddEvent() throws Exception {
+
+    }
+}
---
+++ tests/siat/courant/stream/StreamHubTest.java
@@ -0,0 +1,19 @@
+package siat.courant.stream;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by asus on 2015/6/15.
+ */
+public class StreamHubTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testRegisterStream() throws Exception {
+
+    }
+}
